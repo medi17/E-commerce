f4c3e403dce7107642256fbf083da1452bf574b4
medi17 <medanabebe17@gmail.com>
2026-03-26 00:36:36 +0300

    added hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
 
 const AppProviders = ({children}) => {
   return (
     <>
       {children}
+      <Toaster position="top-right" /> 
     </>
   )
 }
