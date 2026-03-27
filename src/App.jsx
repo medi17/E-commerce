ead309978335551e3bfdc01caf196f91cde11ca7
medi17 <medanabebe17@gmail.com>
2026-03-27 22:17:51 +0300

    added tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Products from './pages/products'
 import AuthPage from './pages/authPage'
 import Admin from './pages/admin'
 import CreateProduct from './pages/createProduct'
+import ProductsDashboard from './components/dashboard/ProductsDashboard'
 
 
 const App = createBrowserRouter([
@@ -27,6 +28,10 @@ const App = createBrowserRouter([
     path: '/createproduct',
     element: <CreateProduct/>    
   },  
+  {
+    path: '/dashboard',
+    element: <ProductsDashboard />
+  }
 ])
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'react-hot-toast'
+import { queryClient } from './lib/queryClient.js'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const AppProviders = ({children}) => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       {children}
-      <Toaster position="top-right" /> 
-    </>
+      <Toaster position="top-right" />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
